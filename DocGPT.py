8453146e6945f04d2b056de9ec17121be283e89f
tauquirahmed <tauquirahmed9@gmail.com>
2024-03-01 14:40:45 +0530

    fixed the llm changing issue

--- DocGPT.py
+++ DocGPT.py
@@ -23,8 +23,9 @@ def saveDoc(pdf_docs):
         with open(os.path.join("data", uploadedfile.name), "wb") as f:
             f.write(uploadedfile.getbuffer())
 
+
 def deleteDoc():
-    folder = 'data'
+    folder = "data"
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
         try:
@@ -33,7 +34,8 @@ def deleteDoc():
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
+
 
 def index_documents(pdf_docs):
     saveDoc(pdf_docs)
@@ -42,10 +44,11 @@ def index_documents(pdf_docs):
     deleteDoc()
     return index
 
+
 def chatBot():
     if "messages" not in st.session_state:
         st.session_state.messages = []
-    
+
     if "query_engine" not in st.session_state:
         st.session_state.query_engine = None
 
@@ -53,41 +56,53 @@ def chatBot():
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-
     if prompt := st.chat_input("What is up?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
-        
+
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
             full_response = st.session_state.query_engine.query(prompt)
 
             message_placeholder.markdown(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": full_response}
+        )
 
 
 def main():
     st.set_page_config("Chat Documents")
     st.header("Chat with Multiple Documents(.pdf, .doc, .csv, etc) using LlamaIndex🦙")
 
-    option = st.selectbox('Select which LLM Service You want to use?',
-    ('gpt-3.5-turbo', 'gemini-pro', 'gpt-4'))
+    option = st.selectbox(
+        "Select which LLM Service You want to use?",
+        ("gpt-3.5-turbo", "gemini-pro"),
+    )
 
-    llm = OpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=256, api_key=os.environ.get('OPENAI_API_KEY'))
+    if "llm" not in st.session_state:
+        st.session_state.llm = OpenAI(
+            model="gpt-3.5-turbo",
+            temperature=0,
+            max_tokens=256,
+            api_key=os.environ.get("OPENAI_API_KEY"),
+        )
 
     if st.button("Select"):
-        if option == 'gemini-pro':
-            llm = genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'), client_options={"api_endpoint": "generativelanguage.googleapis.com"})
-        elif option == 'gpt-4':
-            llm = OpenAI(model="gpt-4", temperature=0, max_tokens=256, api_key=os.environ.get('OPENAI_API_KEY'))
+        if option == "gemini-pro":
+            st.session_state.llm = genai.configure(
+                api_key=os.environ.get("GOOGLE_API_KEY"),
+                client_options={"api_endpoint": "generativelanguage.googleapis.com"},
+            )
 
-
-    Settings.llm = llm
+    Settings.llm = st.session_state.llm
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        pdf_docs = st.file_uploader(
+            "Upload your PDF Files and Click on the Submit & Process Button",
+            accept_multiple_files=True,
+        )
         if st.button("Submit & Process"):
             with st.spinner("Processing..."):
                 if "query_engine" not in st.session_state:
@@ -98,6 +113,7 @@ def main():
                     st.session_state.query_engine = indexes.as_query_engine()
 
     chatBot()
+
 
 if __name__ == "__main__":
     main()
